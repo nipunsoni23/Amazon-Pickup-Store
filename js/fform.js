1e2e0b651120bab56eee77ca3f4bcd351e6b33be
nipun soni <nipunsoni23@gmail.com>
2019-03-21 21:52:12 +0530

    added suggestion box

--- js/fform.js
+++ js/fform.js
@@ -1,6 +1,17 @@
 function giverating(j, i) {
-
   console.log(i);
-  document.getElementById(j).("checked") = true;
+  switch (i) {
+    case 1:
+      document.getElementById("rev" + j).innerHTML = "Very Poor";
+      break;
+    case 2:
+      document.getElementById("rev" + j).innerHTML = "Poor"; break;
+    case 3:
+      document.getElementById("rev" + j).innerHTML = "Satisfactory"; break;
+    case 4:
+      document.getElementById("rev" + j).innerHTML = "Good"; break;
+    case 5:
+      document.getElementById("rev" + j).innerHTML = "Excellent"; break;
 
-}
+  }
+} 
